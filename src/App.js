e1b7e83dc639c3502c0eb329943b50f1e5be0f0e
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-06-01 20:31:50 +0200

    set up Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactGA from 'react-ga';
+
+//Google Analytics code
+const ANALYTICS_KEY = process.env.REACT_APP_ANALYTICS_KEY; // Google analytics API key
+ReactGA.initialize(ANALYTICS_KEY);
+// ReactGA.initialize(ANALYTICS_KEY, { testMode: true });
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
