52b048d7e1c42c1674b3e037103777ac2b3c1c96
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-06-02 20:59:54 +0200

    create App and Home components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {ThemeProvider} from 'styled-components';
 import ReactGA from 'react-ga';
+
+import Home from './Home/Home';
 
 //Google Analytics code
 const ANALYTICS_KEY = process.env.REACT_APP_ANALYTICS_KEY; // Google analytics API key
@@ -8,10 +12,58 @@ ReactGA.initialize(ANALYTICS_KEY);
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+    // Theme to be used in the project, two primary colors depending on redux state
+    const theme = {
+        //colours
+        darkColor: '#0f0f0f',
+        lightColor: '#bbbbbb',
+        background: 'rgba(40,40,40,0.1)',
+
+        //fonts
+        sansSerif: `'Maven Pro', sans-serif`,
+        serif: `'Roboto Slab', serif`,
+        cursive: `'Molle', cursive`,
+
+        //font sizes
+        titleSize: '5em',
+        subtitleSize: '1.5em',
+        bodySize: '1.3em',
+        captionSize: '1em',
+        smallCaptionSize: '0.8em',
+
+        //breakpoints
+        extraSmallScr: '480px',
+        smallScr: '736px',
+        mediumScr: '980px',
+        largeScr: '1280px',
+        extraLargeScr: '1690px',
+
+        //opacity
+        slightlyTranslucent: '0.7',
+        translucent: '0.5',
+        transparent: '0.1',
+
+        //animations
+        pulsateAnimation: '0.8s ease-in-out infinite',
+        blurAnimation: '6s ease-out alternate infinite',
+        popAnimation: '0.5s ease-in-out 1',
+        fadeInAnimation: '0.7s ease-in-out 1',
+
+        //blur filter
+        lightBlur: '3px',
+        heavyBlur: '8px',
+
+        //spacing
+        navIconMargin: '0.1em',
+        navIconPadding: '0.6em'
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Home/>
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/Home/Home.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import {bindActionCreators} from "redux";
+import {Route, Switch, withRouter} from 'react-router-dom';
+import {Redirect} from 'react-router';
+import {connect} from 'react-redux';
+import {createGlobalStyle} from 'styled-components';
+
+/* STYLED COMPONENTS */
+const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: ${props => props.theme.sansSerif};
+        color: ${props => props.theme.darkColor};
+        background: linear-gradient(145deg, ${props => props.theme.color1}, ${props => props.theme.color2});
+        min-height: 100vh;
+        }
+        
+    a {
+        text-decoration: none;
+        color: ${props => props.theme.darkColor};
+
+    }
+     
+     ::selection {
+        color: ${props => props.theme.color1};
+        background-color: ${props => props.theme.darkColor};
+     }
+`;
+
+const Home = (props) => {
+
+    return (
+        <React.Fragment>
+            <GlobalStyle />
+            <div>
+                Home
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Home;
