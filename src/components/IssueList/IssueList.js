4d03c88d36bbc4e347f7453b5516c80c7820b937
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-06-13 20:36:49 +0200

    add function to retrieve contennt from fetched texts

--- src/components/IssueList/IssueList.js
+++ src/components/IssueList/IssueList.js
@@ -12,7 +12,49 @@ const IssueList = (props) => {
     //Wordpress API URL
     const WP_API_URL = process.env.REACT_APP_WP_API_URL;
 
+    //Processes fetched Wordpress data into an array containing texts devided into issues
+    const processTexts = (texts) => {
 
+        //reverse the array so that the oldest texts are first
+        texts.reverse();
+
+        //variable to temporarily hold texts
+        let temporaryTexts = [];
+
+        //variable to hold texts divided into issues
+        let issues = [];
+
+        //variable specifying the current issue while looping through texts
+        let currentIssue = 1;
+
+        for (const text of texts) {
+            //Retrieve issue number and text title from
+            // Split the Wordpress post title which has the format:
+            // X_Some text, at "_" and retrieve
+            // the issue number (X) and text title (Some text)
+            const [issueNo, title] = text.title.rendered.split("_");
+
+            //content of the text
+            const content = text.content.rendered;
+
+            //date of publication
+            const date = text.date;
+
+            //slug pointing to the text
+            const slug = text.slug;
+
+            //add text data to the temporary text template
+            temporaryTexts.push(
+                {
+                    title,
+                    content,
+                    slug
+                }
+            );
+        }
+
+      console.log(temporaryTexts);
+    };
 
     useEffect(() => {
 
@@ -20,8 +62,8 @@ const IssueList = (props) => {
         async function loadTexts() {
             fetch(WP_API_URL + WP_URL_FRAGMENT).then(response => {
                 return response.json();
-            }).then(issues => {
-                console.log(issues);
+            }).then(texts => {
+                processTexts(texts);
             }).catch(err => {
             });
         }
