6df63a4cd4c27ed42af55934ca41ccf3ab050317
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-06-14 12:53:56 +0200

    set up Redux store

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
+import {connect} from 'react-redux';
 import {bindActionCreators} from "redux";
 import {Route, Switch, withRouter} from 'react-router-dom';
 import {Redirect} from 'react-router';
-import {connect} from 'react-redux';
 import {createGlobalStyle} from 'styled-components';
 
 import IssueList from '../IssueList/IssueList';
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(reducer);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const app = (
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>);
+
+ReactDOM.render(app, document.getElementById('root'));
+
 serviceWorker.unregister();
