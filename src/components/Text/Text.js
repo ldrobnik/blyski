e73852327b595d055fa55738e43c48f2586fdcb4
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-06-27 16:34:36 +0200

    fix bug preventing from redirecting non-existing routes

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import {useHistory, Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import styled from 'styled-components';
 
@@ -8,42 +8,46 @@ const Text = (props) => {
 
     let history = useHistory();
 
-
-
     //based on the url, checks which text from which issue should be displayed
     const checkTextID = (issue, slug) => {
 
-        //loop through the text of a given issue and return its idea if there's a match
-        for (let i = 0; i < props.issues[issue - 1].texts.length; i++) {
-            if (props.issues[issue - 1].texts[i].slug === slug) {
-                return i;
+        console.log (issue, slug, props.issues.length);
+            //loop through the text of a given issue and return its idea if there's a match
+            for (let i = 0; i < props.issues[issue - 1].texts.length; i++) {
+                if (props.issues[issue - 1].texts[i].slug === slug) {
+                    return i;
+                }
             }
-        }
 
         //if there's no match, return '-1'
         return -1;
     };
 
+
     //get issue number based on url parameter
     let issueNumber = props.match.params.issue;
 
-    //get text id based on url parameters
-    let textID = checkTextID(issueNumber, props.match.params.slug);
+    //if issue number doesn't exceed array length get text id based on url parameters
+    let textID = (issueNumber <= props.issues.length) ? checkTextID(issueNumber, props.match.params.slug) : -1;
 
     useEffect(() => {
 
-        console.log(issueNumber, textID);
 
-        //if the issue and slug don't match fetched data, redirect to home page
-        if (textID === -1 && props.pageLoaded) {
-         history.push('/');
+        if (props.pageLoaded) {
+
+            //if the issue and slug don't match fetched data, redirect to home page
+            if (textID === -1) {
+                history.push('/');
+            }
+
         }
+
 
     });
 
     return (
         <React.Fragment>
-            {props.pageLoaded &&
+            {(props.pageLoaded && (textID !== -1)) &&
             <React.Fragment>
                 <h3>{props.issues[issueNumber - 1].texts[textID].title}</h3>
                 <div
