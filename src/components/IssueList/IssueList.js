aa907d21bf17f65d34bbb949d6f684bfccaa439a
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-06-27 15:54:10 +0200

    update manifest.json data

--- src/components/IssueList/IssueList.js
+++ src/components/IssueList/IssueList.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import styled from 'styled-components';
 import moment from 'moment';
