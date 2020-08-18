551b017af6958eec8a8db563dda2e0bf773ea2c5
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-08-18 18:48:22 +0200

    simplify Logo.js structure

--- src/components/UI/Logo/Logo.js
+++ src/components/UI/Logo/Logo.js
@@ -1,50 +1,20 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
 
-import {ReactComponent as LogoFrame1} from '../../../assets/images/glider1.svg';
-import {ReactComponent as LogoFrame2} from '../../../assets/images/glider2.svg';
-import {ReactComponent as LogoFrame3} from '../../../assets/images/glider3.svg';
-import {ReactComponent as LogoFrame4} from '../../../assets/images/glider4.svg';
+import logoFrame1 from '../../../assets/images/glider1.svg';
+import logoFrame2 from '../../../assets/images/glider2.svg';
+import logoFrame3 from '../../../assets/images/glider3.svg';
+import logoFrame4 from '../../../assets/images/glider4.svg';
 
-/* STYLED COMPONENTS */
-const LogoWrapper = styled.div`
-  width: 100px;
-  height: 100px;
-  
-  @media all and (max-width: 552px) {
-      height: 60px;
-      width: 60px;
-    }
-    
-  @media all and (max-width: 350px) {
-      height: 40px;
-      width: 40px;
-    }
-  
-  img {
-    height: 100px;
-    width: 100px;
-    
-    @media all and (max-width: 552px) {
-      height: 60px;
-      width: 60px;
-    }
-    
-    @media all and (max-width: 350px) {
-      height: 40px;
-      width: 40px;
-    }
-  }
-`;
 
 const Logo = props => {
 
     //array containing all logo components
-    const logo = [<LogoFrame1/>, <LogoFrame2/>, <LogoFrame3/>, <LogoFrame4/>];
+    const logo = [logoFrame1, logoFrame2, logoFrame3, logoFrame4];
     return (
-            <LogoWrapper>
-                {logo[props.logoFrame]}
-            </LogoWrapper>
+                <object data={logo[props.logoFrame]} width="100px" type="image/svg+xml">
+                    błyski – logo
+                </object>
     );
 };
 
