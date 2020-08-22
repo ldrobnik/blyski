2e24ebcf0cf63e257572ff12d1c60c1940d65a0b
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-08-22 18:43:13 +0200

    add logo styles

--- src/components/UI/Logo/Logo.js
+++ src/components/UI/Logo/Logo.js
@@ -6,15 +6,21 @@ import logoFrame2 from '../../../assets/images/glider2.svg';
 import logoFrame3 from '../../../assets/images/glider3.svg';
 import logoFrame4 from '../../../assets/images/glider4.svg';
 
+const LogoWrapper = styled.div`
+  padding: 12px;
+  border: 10px solid ${props => props.theme.themeColor};
+`;
 
 const Logo = props => {
 
     //array containing all logo components
     const logo = [logoFrame1, logoFrame2, logoFrame3, logoFrame4];
     return (
-                <object data={logo[props.logoFrame]} width="100px" type="image/svg+xml">
-                    błyski – logo
-                </object>
+        <LogoWrapper>
+            <object data={logo[props.logoFrame]} width="100px" type="image/svg+xml">
+                błyski – logo
+            </object>
+        </LogoWrapper>
     );
 };
 
