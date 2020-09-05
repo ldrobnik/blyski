47867a93f44eba5fd4c2ad5b204fb60a0f00844b
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-09-05 11:01:13 +0200

    add basic styling to logo in AboutPanel

--- src/components/About/AboutModal/AboutPanel.js
+++ src/components/About/AboutModal/AboutPanel.js
@@ -37,12 +37,64 @@ const AboutBox = styled.div`
   }
 `;
 
+const Title = styled.div`
+
+width: 100%;
+padding: 10px;
+display: flex;
+align-items: center;
+justify-content: center;
+
+@media all and (max-width: 552px) {
+      padding: 5px;
+    }
+`;
+
+const IssueName = styled.div`
+  font-size: 6em;
+  font-weight: bold;
+  padding-left: 10px;
+  transform: translateY(-8px);
+  
+  @media all and (max-width: 1000px) {
+  font-size: 4em;
+  padding-left: 8px;
+  }
+  
+  @media all and (max-width: 700px) {
+  font-size: 3em;
+  padding-left: 6px;
+  }
+  
+  @media all and (max-width: 552px) {
+      font-size: 2em;
+      padding-left: 4px;
+    }
+    
+  @media all and (max-width: 380px) {
+      font-size: 1.5em;
+      padding-left: 4px;
+    }
+    
+  @media all and (max-width: 350px) {
+      font-size: 1em;
+      padding-left: 4px;
+    }
+`;
+
 const AboutPanel = props => {
 
     return (
         <AboutBox>
-            <Logo inverted={true}/>
-            <h1>{WEBSITE_TEXT.about.title}</h1>
+            <Title>
+                <Logo
+                    logoFrame={1}
+                    inverted={true}/>
+                    <IssueName>
+                        {WEBSITE_TEXT.about.title}
+                    </IssueName>
+            </Title>
+
             <h3>{WEBSITE_TEXT.about.subtitle}</h3>
             {WEBSITE_TEXT.about.content}
             <p>{WEBSITE_TEXT.about.contact}</p>
