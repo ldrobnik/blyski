401f2c3a4756844ae4d5a3f17b3649ace0fe8654
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-09-06 12:52:57 +0200

    create Story & Bio components

---
+++ src/components/Text/Bio/Bio.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+
+`;
+
+const Bio = (props) => {
+
+    return (
+        <StyledWrapper>
+
+        </StyledWrapper>
+    );
+};
+
+export default Bio;
---
+++ src/components/Text/Story/Story.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+
+`;
+
+const Story = (props) => {
+
+    return (
+        <StyledWrapper>
+
+        </StyledWrapper>
+    );
+};
+
+export default Story;
