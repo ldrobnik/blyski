7bd044676fb48a9d69a89f7bcd2bc9ac8939eac8
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-04-17 10:22:39 +0200

    update fallback issue

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -163,7 +163,7 @@ const Home = (props) => {
             //process the data to organise texts into issues
             const issueData = processTexts(texts);
 
-            console.log(issueData);
+            // console.log(issueData);
 
             //update the store with the organised issue data
             updateIssues(issueData);
