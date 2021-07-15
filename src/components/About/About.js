38fd59bbbdb1933f57c403545e5cb953a89d55a1
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-07-15 10:30:44 +0200

    move styled-components code from About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,35 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
-import styled from 'styled-components';
+import {AboutPanelWrapper, Backdrop} from "../../styled";
 import AboutPanel from './AboutPanel/AboutPanel';
 import {WEBSITE_TEXT, AnimatedContent, fadeTimeout} from "../../data/constants";
-
-/* STYLED COMPONENTS */
-
-const AboutPanelWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-
-  top: 0;
-  left: 0;
-  
-   *::selection {
-        color: ${props => props.theme.themeColor};
-        background-color: ${props => props.theme.lightColor};
-     }
-`;
-
-const Backdrop = styled.div`
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100vw;
-  height: 100vh;
-  background: ${props => props.theme.themeColor};
-`;
 
 const About = (props) => {
 
--- src/styled.js
+++ src/styled.js
@@ -1,2 +1,30 @@
 /* STYLED-COMPONENTS */
 
+import styled from 'styled-components';
+
+/* About.js */
+
+export const AboutPanelWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+
+  top: 0;
+  left: 0;
+  
+   *::selection {
+        color: ${props => props.theme.themeColor};
+        background-color: ${props => props.theme.lightColor};
+     }
+`;
+
+export const Backdrop = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background: ${props => props.theme.themeColor};
+`;
