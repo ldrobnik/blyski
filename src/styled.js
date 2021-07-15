e7c7b6793a51439f75bf4389dbdbe1fc7369e7d8
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-07-15 15:44:55 +0200

    move styled-components code from InactiveButton.js

--- src/styled.js
+++ src/styled.js
@@ -915,3 +915,91 @@ color: ${props => props.theme.themeColor};
     }
 
 `;
+
+/* InactiveButton.js */
+
+export const InactiveButtonWrapper = styled.div`
+font-weight: bold;
+font-size: 1.2em;
+color: ${props => props.theme.themeColor};
+display: inline-block;
+position: relative;
+margin: 1px 0;
+padding: 10px 20px;
+overflow: hidden;
+transition: all 0.2s ease-in;
+z-index: 50;
+
+color: ${props => props.theme.lightColor};
+
+&:before {
+  content: '';
+  background-color: ${props => props.theme.themeColor};
+  position: absolute;
+  width: 120%;
+  height: 120%;
+  left: 0;
+  top: 0;
+  z-index: -20;
+  transform: translateY(-2px);
+  transition: all 0.2s ease-in;
+}
+
+ 
+  @media all and (max-width: ${props => props.theme.mediumScr}) {
+  font-size: 1.1em;
+  padding: 10px 10px;
+  }
+
+  @media all and (max-width: ${props => props.theme.smallScr}) {
+      font-size: 1em;
+      padding: 10px 6px;
+    }
+    @media all and (max-width: ${props => props.theme.tinyScr}) {
+      font-size: 0.9em;
+      padding: 6px 4px;
+    }
+`;
+
+export const InvertedInactiveButtonWrapper = styled.div`
+font-weight: bold;
+font-size: 1.2em;
+color: ${props => props.theme.themeColor};
+display: inline-block;
+position: relative;
+margin: 1px 0;
+padding: 10px 20px;
+overflow: hidden;
+transition: all 0.2s ease-in;
+z-index: 50;
+
+&:before {
+  content: '';
+  background-color: ${props => props.theme.lightColor};
+  position: absolute;
+  width: 120%;
+  height: 120%;
+  left: 0;
+  top: 0;
+  z-index: -20;
+  transform: translateY(-2px);
+  transition: all 0.2s ease-in;
+}
+
+
+  @media all and (max-width: ${props => props.theme.mediumScr}) {
+  font-size: 1.1em;
+  padding: 10px 10px;
+  }
+
+  @media all and (max-width: ${props => props.theme.smallScr}) {
+      font-size: 1em;
+      padding: 10px 6px;
+    }
+    @media all and (max-width: ${props => props.theme.tinyScr}) {
+      font-size: 0.9em;
+      padding: 6px 4px;
+    }
+
+`;
+
