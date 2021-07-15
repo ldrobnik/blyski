e896e76b8dd41946261a7d38a50fd525ece48106
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-07-15 15:43:24 +0200

    move styled-components code from HoverableButton.js

--- src/styled.js
+++ src/styled.js
@@ -817,3 +817,101 @@ export const ErrorMessageContent = styled.div`
     
   }
 `;
+
+/* HoverableButton */
+
+export const HoverableButtonWrapper = styled.div`
+font-weight: bold;
+font-size: 1.2em;
+color: ${props => props.theme.themeColor};
+display: inline-block;
+position: relative;
+margin: 1px 0;
+padding: 10px 20px;
+overflow: hidden;
+cursor: pointer;
+transition: all 0.2s ease-in;
+z-index: 50;
+
+&:hover {
+color: ${props => props.theme.lightColor};
+}
+
+&:before {
+  content: '';
+  background-color: ${props => props.theme.themeColor};
+  position: absolute;
+  width: 120%;
+  height: 120%;
+  left: 0;
+  top: 0;
+  z-index: -20;
+  transform: translateY(100%);
+  transition: all 0.2s ease-in;
+}
+
+  &:hover:before {
+  transform: translateY(-2px);
+  }
+
+  @media all and (max-width: ${props => props.theme.mediumScr}) {
+  font-size: 1.1em;
+  padding: 10px 10px;
+  }
+
+    @media all and (max-width: ${props => props.theme.tinyScr}) {
+      font-size: 0.9em;
+      padding: 6px 10px;
+    }
+`;
+
+export const InvertedHoverableButtonWrapper = styled.div`
+font-weight: bold;
+font-size: 1.2em;
+color: ${props => props.theme.lightColor};
+display: inline-block;
+position: relative;
+margin: 1px 0;
+padding: 10px 20px;
+overflow: hidden;
+cursor: pointer;
+transition: all 0.2s ease-in;
+z-index: 50;
+
+&:hover {
+color: ${props => props.theme.themeColor};
+}
+
+&:before {
+  content: '';
+  background-color: ${props => props.theme.lightColor};
+  position: absolute;
+  width: 120%;
+  height: 120%;
+  left: 0;
+  top: 0;
+  z-index: -20;
+  transform: translateY(100%);
+  transition: all 0.2s ease-in;
+}
+
+  &:hover:before {
+  transform: translateY(-2px);
+  }
+
+
+  @media all and (max-width: ${props => props.theme.mediumScr}) {
+  font-size: 1.1em;
+  padding: 10px 10px;
+  }
+
+  @media all and (max-width: ${props => props.theme.smallScr}) {
+      font-size: 1em;
+      padding: 10px 6px;
+    }
+    @media all and (max-width: ${props => props.theme.tinyScr}) {
+      font-size: 0.9em;
+      padding: 6px 4px;
+    }
+
+`;
