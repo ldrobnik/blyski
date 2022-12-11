a27957b990eb188b55cd3b6ae414831b60930b28
Łukasz Drobnik <drobniklukasz@gmail.com>
2022-12-11 15:06:31 +0100

    enable separators in Story.js

--- src/components/Text/Story/Story.js
+++ src/components/Text/Story/Story.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import Markdown from 'markdown-to-jsx/dist/index.js';
-import {WEBSITE_TEXT} from '../../../data/constants';
-import {StoryTextTitle, StoryAuthor, StoryTranslatedBy, StoryTextBody, StoryCredits} from '../../../styled';
+import React from "react";
+import Markdown from "markdown-to-jsx/dist/index.js";
+import { WEBSITE_TEXT } from "../../../data/constants";
+import Separator from "../../UI/Separator/Separator";
+import {
+  StoryTextTitle,
+  StoryAuthor,
+  StoryTranslatedBy,
+  StoryTextBody,
+  StoryCredits,
+} from "../../../styled";
 
-const Story = props => {
-
-    return (
-        <React.Fragment>
-            <StoryAuthor>{props.author}</StoryAuthor>
-            <StoryTextTitle>{props.textTitle}</StoryTextTitle>
-            <StoryTranslatedBy>{WEBSITE_TEXT.issueList.translator}</StoryTranslatedBy>
-            <StoryTextBody>
-                <Markdown
-                    options={{
-                        overrides: {
-                            credits: {
-                                component: StoryCredits
-                            },
-                        }
-                    }}
-                >
-                    {props.textContent}
-                </Markdown>
-            </StoryTextBody>
-        </React.Fragment>
-    );
+const Story = (props) => {
+  return (
+    <React.Fragment>
+      <StoryAuthor>{props.author}</StoryAuthor>
+      <StoryTextTitle>{props.textTitle}</StoryTextTitle>
+      <StoryTranslatedBy>{WEBSITE_TEXT.issueList.translator}</StoryTranslatedBy>
+      <StoryTextBody>
+        <Markdown
+          options={{
+            overrides: {
+              separator: {
+                component: Separator,
+              },
+              credits: {
+                component: StoryCredits,
+              },
+            },
+          }}
+        >
+          {props.textContent}
+        </Markdown>
+      </StoryTextBody>
+    </React.Fragment>
+  );
 };
 
 export default Story;
